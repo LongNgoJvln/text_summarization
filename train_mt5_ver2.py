2080ef29516fd4bfb339b2470d4179ee8edfd39c
LongNgoJvln <130882633+LongNgoJvln@users.noreply.github.com>
2026-04-25 02:16:58 +0900

    Update train_mt5_ver2.py

--- train_mt5_ver2.py
+++ train_mt5_ver2.py
@@ -8,6 +8,7 @@ from datasets import load_dataset, disable_progress_bar
 from transformers import (
     AutoTokenizer,
     AutoModelForSeq2SeqLM,
+    MT5ForConditionalGeneration,
     DataCollatorForSeq2Seq,
     Seq2SeqTrainer,
     Seq2SeqTrainingArguments,
@@ -19,7 +20,7 @@ import warnings
 warnings.filterwarnings("ignore")
 disable_progress_bar()
 
-MODEL_NAME = "csebuetnlp/mT5_multilingual_XLSum"
+MODEL_NAME = "google/mt5-small"
 EBS_MODEL_DIR = "/mnt/ebs/mt5_xlsum_vi"
 EBS_JSON_PATH = "/mnt/ebs/results/"
 # LOCAL_JSON_PATH = "./evaluation_results.json"
@@ -40,22 +41,27 @@ print("Loading tokenizer...")
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
 max_input_length = 512
-max_target_length = 128
+max_target_length = 80
 
 def preprocess(example):
-    model_input = example["text"]
+    model_input = ["summarize: " + text for text in example["text"]]
     target = example["summary"]
+
+    # model_input = [" ".join(text.split()) for text in example["text"]]
+    # target = [" ".join(summary.split()) for summary in example["summary"]]
 
     inputs = tokenizer(
         model_input,
         max_length=max_input_length,
         truncation=True,
+        padding = "max_length"
     )
-    with tokenizer.as_target_tokenizer():
-        labels = tokenizer(
-            target,
-            max_length=max_target_length,
-            truncation=True,
+
+    labels = tokenizer(
+        target,
+        max_length=max_target_length,
+        truncation=True,
+        padding = "max_length"
         )
 
     inputs["labels"] = labels["input_ids"]
@@ -71,14 +77,20 @@ tokenized_dataset = dataset.map(
 )
 data_collator = DataCollatorForSeq2Seq(
     tokenizer,
-    model=MODEL_NAME
+    model=MODEL_NAME,
+    padding = True,
 )
 
 rouge = evaluate.load("rouge")
 
 def compute_metrics(eval_pred):
     preds, labels = eval_pred
-    labels = np.where(torch.tensor(labels) != -100, torch.tensor(labels), torch.tensor(tokenizer.pad_token_id))
+
+    if isinstance(preds, tuple):
+        preds = preds[0]
+
+    preds = np.argmax(preds, axis=-1)
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
     decoded_preds = tokenizer.batch_decode(
         preds,
         skip_special_tokens=True
@@ -95,32 +107,37 @@ def compute_metrics(eval_pred):
     return scores
 
 print("Loading model...")
-model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME)
+model = MT5ForConditionalGeneration.from_pretrained(MODEL_NAME)
 
 training_args = Seq2SeqTrainingArguments(
     output_dir=EBS_MODEL_DIR,
-    learning_rate=1e-5,
+    learning_rate=3e-5,
     per_device_train_batch_size=1,
     per_device_eval_batch_size=1, 
     gradient_accumulation_steps=2,
-    num_train_epochs=1,
-    evaluation_strategy="epoch",
-    save_strategy="epoch",
-    logging_steps=200,
-    fp16=True,
-    load_best_model_at_end=True,
-    report_to="none",
+    num_train_epochs=3,
+    evaluation_strategy="steps",
+    save_strategy="steps",
+    logging_steps=50,
+    eval_steps=2000,
     predict_with_generate=True,
     max_grad_norm=1.0,
-    save_total_limit=1 
+    save_steps=2000,
+    save_total_limit=1,
+    warmup_steps=100,
+    weight_decay=0.01,
+    metric_for_best_model="rouge1",
+    greater_is_better=True,
+    fp16=False,
+    # optim = "adafactor",
 )
 
 
 trainer = Seq2SeqTrainer(
     model=model,
     args=training_args,
-    train_dataset=tokenized_dataset["train"].shuffle(seed=42).select(range(1000)), 
-    eval_dataset=tokenized_dataset["validation"].shuffle(seed=42).select(range(200)), 
+    train_dataset=tokenized_dataset["train"].shuffle(seed=42).select(range(10000)), 
+    eval_dataset=tokenized_dataset["validation"].shuffle(seed=42).select(range(100)), 
     tokenizer=tokenizer,
     data_collator=data_collator,
     compute_metrics=compute_metrics,
@@ -135,14 +152,15 @@ trainer.save_model(EBS_MODEL_DIR)
 tokenizer.save_pretrained(EBS_MODEL_DIR)
 
 print("Running inference on test set...")
-test_data = dataset["test"].select(range(50))
+test_data = dataset["test"].shuffle(seed=42).select(range(100)) 
 
 results = []
 all_preds = []
 all_labels = []
 
 for sample in test_data:
-    input_text = sample["text"]
+    # input_text = " ".join(sample["text"].split())
+    input_text = "summarize: " + sample["text"]
     inputs = tokenizer(
         input_text,
         return_tensors="pt",
@@ -154,6 +172,8 @@ for sample in test_data:
         **inputs,
         max_length = max_target_length,
         num_beams=4,
+        no_repeat_ngram_size=3,
+        length_penalty=1.0,
         early_stopping=True
     )
 
@@ -183,15 +203,15 @@ final_output = {
 }
 
 print("Saving JSON...")
-output_file = os.path.join(EBS_JSON_PATH, "evaluation_fulltext_input.json")
+output_file = os.path.join(EBS_JSON_PATH, "evaluation_fulltext_180426.json")
 with open(output_file, "w", encoding="utf-8") as f:
     json.dump(final_output, f, ensure_ascii=False, indent=4)
 
-print("Compressing model for local download...")
-zip_base_name = os.path.join(EBS_JSON_PATH, "mt5_finetuned_model")
-shutil.make_archive(zip_base_name, 'zip', EBS_MODEL_DIR)
-model_zip_full_path = zip_base_name + ".zip"
-print("Copy model to local...")
-print(json.dumps([output_file, model_zip_full_path]))
+# print("Compressing model for local download...")
+# zip_base_name = os.path.join(EBS_JSON_PATH, "mt5_finetuned_model")
+# shutil.make_archive(zip_base_name, 'zip', EBS_MODEL_DIR)
+# model_zip_full_path = zip_base_name + ".zip"
+# print("Copy model to local...")
+# print(json.dumps([output_file, model_zip_full_path]))
 print(json.dumps([output_file]))
 print("true")
